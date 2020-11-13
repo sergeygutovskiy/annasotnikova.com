b6f763c8781a31eff685683e9d6337d5b2e0eb10
sergeygutovskiy <sergey.gutovskiy@gmail.com>
2020-11-13 17:48:26 +0300

    huy

---
+++ article.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Статья</title>
+	<?php require 'templates/_header.php'; ?>
+	<link rel="stylesheet" type="text/css" href="css/pages/article.css">
+</head>
+<body>
+	<!-- navigation -->
+	<?php require 'templates/_navigation.php'; ?>
+
+	<section class="article-container">
+		<img class="article-logo" src="/imgs/logo.png">
+	</section>
+
+	<header class="article-container">
+		<span class="article__category">lorem ipsum</span>
+		<div class="container__title">
+			<h2 class="article__title">lorem ipsum</h2>
+		</div>
+		<div class="article__image-container image-container image-container_4_1">
+			<img class="cover-image" src="/imgs/announcements-example.jpg">
+		</div>
+	</header>
+
+	<section class="article-container">
+		<div class="article__content">
+			<h2>Lorem ipsum dolor</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			</p>
+			<div class="image-container image-container_8_5">
+				<img class="cover-image" src="/imgs/announcements-example.jpg">
+			</div>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			</p>
+		</div>
+	</section>
+
+	<!-- footer -->
+	<?php require 'templates/_footer.php'; ?>
+</body>
+</html>
