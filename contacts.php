d25e43d47e8e3548d996db5de52d3aab23c8a9f8
sergeygutovskiy <sergey.gutovskiy@gmail.com>
2020-11-13 19:23:43 +0300

    huy

---
+++ contacts.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Контакты</title>
+	<?php require 'templates/_header.php'; ?>
+	<link rel="stylesheet" type="text/css" href="css/pages/contacts.css">
+</head>
+<body>
+	<!-- navigation -->
+	<?php require 'templates/_navigation.php'; ?>
+
+	<section class="contacts-container">
+		<img class="contacts-logo" src="/imgs/logo.png">
+	</section>
+
+	<header class="contacts-container">
+		<div class="container__title">
+			<h2>Контакты</h2>
+		</div>
+	</header>
+
+	<section class="contacts__content contacts-container">
+		<div class="contacts__links">
+			<div class="contacts__link">
+				<span>instagram</span>
+				<span>@sotnikovaanna</span>
+			</div>
+			<div class="contacts__link">
+				<span>vkontakte</span>
+				<span>/sotnikova28</span>
+			</div>
+			<div class="contacts__link">
+				<span>e-mail</span>
+				<span>annasotnikova2803@gmail.com</span>
+			</div>
+		</div>
+		<div class="contacts__footer">
+			<div class="contacts__footer-mailing">
+				<span class="contacts__footer-title">Подписаться на новости:</span>
+				<form>
+					<input class="contacts__footer-input" placeholder="e-mail">
+					<button class="contacts__footer-button"></button>
+				</form>
+				<p class="contacts__footer-text">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				</p>
+			</div>
+			<div class="contacts__footer-links">
+				<div><a href="">Telegram </a></div>
+				<div><a href="">whatsapp </a></div>
+				<div><a href="">facebook </a></div>
+				<div><a href="">youtube  </a></div>
+			</div>
+		</div>
+	</section>
+
+</body>
+</html>
