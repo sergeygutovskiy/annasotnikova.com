0ced3357f08d5a059f825265a59ea6c08f6f9c59
sergeygutovskiy <sergey.gutovskiy@gmail.com>
2020-11-15 20:21:36 +0300

    huy

--- press.php
+++ press.php
@@ -29,92 +29,238 @@
 	<section class="press-container press__tabs-navigation">
 		<div class="tabs-navigation">
 			<div class="tabs-navigation__content">
-				<a class="tabs-navigation__item" href="#" data-count="21">все</a>
-				<a class="tabs-navigation__item" href="#" data-count="5">статьи</a>
-				<a class="tabs-navigation__item" href="#" data-count="15">рецензии</a>
-				<a class="tabs-navigation__item" href="#" data-count="6">Отзывы</a>
-				<a class="tabs-navigation__item" href="#" data-count="10">фотографии</a>
+				<a class="tabs-navigation__item"
+				data-category="all"
+				data-count="21">все</a>
+				<a class="tabs-navigation__item"
+				data-category="articles"
+				data-count="5">статьи</a>
+				<a class="tabs-navigation__item"
+				data-category="reviews"
+				data-count="15">рецензии</a>
+				<a class="tabs-navigation__item"
+				data-category="comments"
+				data-count="6">Отзывы</a>
+				<a class="tabs-navigation__item"
+				data-category="photo"
+				data-count="10">фотографии</a>
 			</div>
 		</div>
 	</section>
 
 	<section class="press-container">
-		<div class="press__content press__content--active" data-category="all">
-			<article class="press-item">
-				<div class="press-item__image-container">
-					<div class="image-container image-container_8_5">
-						<img class="cover-image" src="/imgs/announcements-example.jpg">
+		<div class="press">
+			<div class="press__content" data-category="all">
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
 					</div>
-				</div>
-				<div class="press-item__panel">
-					<span class="press-item__category">статья</span>
-					<a href="/" class="press-item__title">Lorem ipsum dolor</a>
-					<span class="press-item__date">24 сентября, 2020</span>
-				</div>
-			</article>
-			<article class="press-item">
-				<div class="press-item__image-container">
-					<div class="image-container image-container_8_5">
-						<img class="cover-image" src="/imgs/announcements-example.jpg">
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
 					</div>
-				</div>
-				<div class="press-item__panel">
-					<span class="press-item__category">статья</span>
-					<a href="/" class="press-item__title">Lorem ipsum dolor</a>
-					<span class="press-item__date">24 сентября, 2020</span>
-				</div>
-			</article>
-			<article class="press-item">
-				<div class="press-item__image-container">
-					<div class="image-container image-container_8_5">
-						<img class="cover-image" src="/imgs/announcements-example.jpg">
+				</article>
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
 					</div>
-				</div>
-				<div class="press-item__panel">
-					<span class="press-item__category">статья</span>
-					<a href="/" class="press-item__title">Lorem ipsum dolor</a>
-					<span class="press-item__date">24 сентября, 2020</span>
-				</div>
-			</article>
-		</div>
-		<div class="press__content" data-category="articles">
-			<article class="press-item">
-				<div class="press-item__image-container">
-					<div class="image-container image-container_8_5">
-						<img class="cover-image" src="/imgs/announcements-example.jpg">
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
 					</div>
-				</div>
-				<div class="press-item__panel">
-					<span class="press-item__category">статья</span>
-					<a href="/" class="press-item__title">Lorem ipsum dolor</a>
-					<span class="press-item__date">24 сентября, 2020</span>
-				</div>
-			</article>
-			<article class="press-item">
-				<div class="press-item__image-container">
-					<div class="image-container image-container_8_5">
-						<img class="cover-image" src="/imgs/announcements-example.jpg">
+				</article>
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
 					</div>
-				</div>
-				<div class="press-item__panel">
-					<span class="press-item__category">статья</span>
-					<a href="/" class="press-item__title">Lorem ipsum dolor</a>
-					<span class="press-item__date">24 сентября, 2020</span>
-				</div>
-			</article>
-			<article class="press-item">
-				<div class="press-item__image-container">
-					<div class="image-container image-container_8_5">
-						<img class="cover-image" src="/imgs/announcements-example.jpg">
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
 					</div>
-				</div>
-				<div class="press-item__panel">
-					<span class="press-item__category">статья</span>
-					<a href="/" class="press-item__title">Lorem ipsum dolor</a>
-					<span class="press-item__date">24 сентября, 2020</span>
-				</div>
-			</article>
+				</article>
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
+					</div>
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
+					</div>
+				</article>
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
+					</div>
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
+					</div>
+				</article>
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
+					</div>
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
+					</div>
+				</article>												
+			</div>
+			<div class="press__content" data-category="articles">
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
+					</div>
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
+					</div>
+				</article>
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
+					</div>
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
+					</div>
+				</article>
+			</div>	
+			<div class="press__content" data-category="reviews">
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
+					</div>
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
+					</div>
+				</article>
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
+					</div>
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
+					</div>
+				</article>
+			</div>	
+			<div class="press__content" data-category="comments">
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
+					</div>
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
+					</div>
+				</article>
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
+					</div>
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
+					</div>
+				</article>
+			</div>	
+			<div class="press__content" data-category="photo">
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
+					</div>
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
+					</div>
+				</article>
+				<article class="press-item">
+					<div class="press-item__image-container">
+						<div class="image-container image-container_8_5">
+							<img class="cover-image" src="/imgs/announcements-example.jpg">
+						</div>
+					</div>
+					<div class="press-item__panel">
+						<span class="press-item__category">статья</span>
+						<a href="/" class="press-item__title">Lorem ipsum dolor</a>
+						<span class="press-item__date">24 сентября, 2020</span>
+					</div>
+				</article>
+			</div>			
 		</div>
 	</section>
+
+	<script>
+		$(".tabs-navigation__item").eq(0).addClass("tabs-navigation__item--active");
+		$(".press__content").eq(0).addClass("press__content--active");
+		$(".press__content").eq(0).css("display", "flex");
+
+		$(".tabs-navigation__item").click(function() {
+			let active = $(".press__content--active");
+			let current = $(this);
+
+			active.removeClass("press__content--active")
+			$(".tabs-navigation__item--active").removeClass("tabs-navigation__item--active");
+
+			$(".tabs-navigation__item[data-category='" 
+				+ current.attr("data-category") + "']")
+				.addClass("tabs-navigation__item--active");
+
+			setTimeout(function() {
+				active.hide();
+				$(".press__content[data-category='" + current.attr("data-category") + "']")
+					.css("display", "flex");
+			}, 300);
+
+			setTimeout(function() {
+				$(".press__content[data-category='" + current.attr("data-category") + "']")
+					.addClass("press__content--active");
+			}, 330);
+		});
+	</script>
+
+	<!-- footer -->
+	<?php require 'templates/_footer.php'; ?>
 </body>
 </html>
