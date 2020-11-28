4329b5262f58f0f3f54666a090b36ed90320d276
sergeygutovskiy <sergey.gutovskiy@gmail.com>
2020-11-28 17:30:09 +0300

    huy

---
+++ search.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Поиск</title>
+	<?php require 'templates/_header.php'; ?>
+	<link rel="stylesheet" type="text/css" href="css/pages/search.css">
+</head>
+<body>
+	<!-- navigation -->
+	<?php require 'templates/_navigation.php'; ?>
+
+	<section class="search search-container">
+		<div class="search__title">
+			<span>ПОИСК ПО:</span>
+			учебник				
+		</div>
+		<div class="search__content">
+			<div class="search-result">
+				<div class="search-result__category">category</div>
+				<div class="search-result__bottom">
+					<span class="search-result__title">Учебник официанта</span>
+					<a href="">подробнее</a>
+				</div>
+			</div>
+			<div class="search-result">
+				<div class="search-result__category">category</div>
+				<div class="search-result__bottom">
+					<span class="search-result__title">Учебник официанта</span>
+					<a href="">подробнее</a>
+				</div>
+			</div>
+			<div class="search-result">
+				<div class="search-result__category">category</div>
+				<div class="search-result__bottom">
+					<span class="search-result__title">Учебник официанта</span>
+					<a href="">подробнее</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- footer -->
+	<?php require 'templates/_footer.php'; ?>
+</body>
+</html>
